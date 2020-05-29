4fa13ead44a3b5f1cc7046a98701e67d4bb7eb91
Neda Rohani <sonia@Nedas-MacBook-Pro.local>
2020-05-29 15:42:59 -0700

    add unit test

---
+++ src/components/Post.test.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import Post from './Post';
+import { shallow } from 'enzyme';
+
+describe('Post', () => {
+  const mockProps = {id: 1, title: 'bar', body:'foo', userId:2};
+  const wrapper = shallow(<Post post={mockProps} key={mockProps.id}/>);
+  it('should render without errors', () => {
+    const sectionClass = wrapper.find('.post-excerpt');
+    expect(sectionClass.length).toBe(1)
+  });
+
+  it('should render a text header', () => {
+    const header = wrapper.find('h3');
+    expect(header.length).toBe(1)
+  });
+
+  it('should render the body of the post',() => {
+    const body = wrapper.find('p');
+    expect(body.length).toBe(1)
+  });
+
+})
